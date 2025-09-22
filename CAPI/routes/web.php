ef86970f2fdadf13e0cddb5de74d1fb5d7dbaefb
zohaibkhan101 <zohaibkhan2352@gmail.com>
2025-09-22 14:30:05 +0300

    route added

--- CAPI/routes/web.php
+++ CAPI/routes/web.php
@@ -16,5 +16,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::middleware(['auth','is_admin'])->group(function () {
+    Route::get('/admin', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+
+    Route::resource('admin/careers', \App\Http\Controllers\CareerController::class);
+    Route::get('admin/vendors', [\App\Http\Controllers\VendorController::class, 'index'])->name('admin.vendors');
+});
 
 require __DIR__.'/auth.php';
